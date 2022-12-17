337b42946a3345144c7f41a270444ae97e184b43
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-12-17 16:11:35 +0800

    Add files via upload

--- calc.py
+++ calc.py
@@ -2,19 +2,12 @@ import tkinter as tk
 import matplotlib.pyplot as plt
 
 from sympy import *
-
 from decimal import Decimal
-
 from functools import partial
-
-from numpy import e
-from numpy import pi
-from numpy import linspace
+from numpy import e, pi, linspace
 from numpy import real as Re
 from numpy import imag as Im
-
-from scipy.special import erf
-from scipy.special import zeta
+from scipy.special import erf, zeta
 from scipy.special import lambertw as W
 
 root = tk.Tk()
@@ -22,7 +15,7 @@ root.geometry("450x600")
 root.config(bg = '#121212')
 root.wm_attributes('-alpha', '0.9')
 
-text = tk.Text(root, height=5, width=60, fg = '#ffffff', bg='#2c2c2b')
+text = tk.Text(root, height=5, width=60, fg='#ffffff', bg='#2c2c2b')
 text.bind("<Key>", lambda e : "break")
 text.pack()
 
@@ -135,6 +128,75 @@ class Buttons:
         text.insert(tk.END, "^")
         self.key_buffer += "**"
 
+    # mark
+    global senabled, henabled
+    senabled = False
+    henabled = False
+
+    def shift(self):
+        global senabled
+        senabled = True
+        return senabled
+
+    def hyperbolic(self):
+        global henabled
+        henabled = True
+        return henabled
+
+    def sin(self):
+        global senabled, henabled
+        if senabled and not henabled:
+            self.a_function('arcsin', 'asin')
+            senabled = False
+            return senabled
+        elif henabled and not senabled:
+            self.a_function('sinh', 'sinh')
+            henabled = False
+            return henabled
+        elif senabled and henabled:
+            self.a_function('arcsinh', 'asinh')
+            senabled = False
+            henabled = False
+            return senabled, henabled
+        else:
+            self.a_function('sin', 'sin')
+
+    def cos(self):
+        global senabled, henabled
+        if senabled and not henabled:
+            self.a_function('arccos', 'acos')
+            senabled = False
+            return senabled
+        elif henabled and not senabled:
+            self.a_function('cosh', 'cosh')
+            henabled = False
+            return henabled
+        elif senabled and henabled:
+            self.a_function('arccosh', 'acosh')
+            senabled = False
+            henabled = False
+            return senabled, henabled
+        else:
+            self.a_function('cos', 'cos')
+
+    def tan(self):
+        global senabled, henabled
+        if senabled and not henabled:
+            self.a_function('arctan', 'atan')
+            senabled = False
+            return senabled
+        elif henabled and not senabled:
+            self.a_function('tanh', 'tanh')
+            henabled = False
+            return henabled
+        elif senabled and henabled:
+            self.a_function('arctanh', 'atanh')
+            senabled = False
+            henabled = False
+            return senabled, henabled
+        else:
+            self.a_function('tan', 'tan')
+
     def equals(self, print_res=True, plot=False):
         if len(self.key_buffer) == 0: return
         if plot and print_res:
@@ -192,7 +254,13 @@ class Buttons:
         self.equals(print_res=False, plot=True)
 
     def delete(self):
-        full_delete_kw = {"ln(" : "ln(", "log(" : "log(", "√(" : "sqrt(", "factorial(" : "factorial(", " + " : "+", " - " : "-", " × " : "*", " / " : "/"}
+        full_delete_kw = {
+            "arcsinh(" : "asinh(", "arcsin(" : "asin(", "sin(" : "sin(",  "sinh(" : "sinh(",
+            "arccosh(" : "acosh(", "arccos(" : "acos(", "cosh(" : "cosh(", "cos(" : "cos(",
+            "arctanh(" : "atanh(", "arctan(" : "atan(", "tanh(" : "tanh(", "tan(" : "tan(",
+            "ln(" : "ln(", "log(" : "log(", "√(" : "sqrt(", "abs(" : "abs(", "factorial(" : "factorial(",
+            " + " : "+", " - " : "-", " × " : "*", " / " : "/"
+            }
         for i in full_delete_kw.keys():
             if text.get("1.0", "end-1c").endswith(i):
                 text.delete(f"end-{len(i) + 1}c", tk.END)
@@ -209,17 +277,25 @@ class Buttons:
 
     def init(self):
         # colors for buttons
-        black = '#8f8f8f'
-        red = '#e91e63'
+        #black = '#8f8f8f'
+        black = '#302535'
+        white = '#ffffff'
+        red = '#ff0000'
+        yellow = '#ffba01'
+        blue = '#58cced'
 
-        lblack = '#696969'
+        #lblack = '#696969'
+        lblack = '#2c252f'
+        lwhite = '#666666'
         lred = '#ad1146'
+        lyellow = '#c29200'
+        lblue = '#0492c2'
         # not so nice colors
 
-        coords = [(10, 220), (10, 180), (70, 180), (130, 180), (10, 140), (70, 140), (130, 140), (10, 100), (70, 100), (130, 100)]
+        coords = [(10, 260), (10, 220), (70, 220), (130, 220), (10, 180), (70, 180), (130, 180), (10, 140), (70, 140), (130, 140)]
         for i in range(9, -1, -1):
             f = partial(self.a, i)
-            locals()[f"button{i}"] = tk.Button(root, text=i, command=f, height=1, width=3, bg=black, activebackground=lblack)
+            locals()[f"button{i}"] = tk.Button(root, text=i, command=f, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
             locals()[f"button{i}"].place(x=coords[i][0], y=coords[i][1])
 
         op = partial(self.a, "(")
@@ -228,50 +304,70 @@ class Buttons:
         _pi = partial(self.a, pi)
         _x = partial(self.a, "x")
         cm = partial(self.a, ",")
+        abs = partial(self.a, "abs(")
 
-        button_dot = tk.Button(root, text=".", command=self.dot, height=1, width=3, bg=black, activebackground=lblack)
-        button_sign = tk.Button(root, text="+/-", command=self.sign, height=1, width=3, bg=black, activebackground=lblack)
-        button_plus = tk.Button(root, text="+", command=self.plus, height=1, width=3, bg=black, activebackground=lblack)
-        button_minus = tk.Button(root, text="-", command=self.minus, height=1, width=3, bg=black, activebackground=lblack)
-        button_mult = tk.Button(root, text="×", command=self.mult, height=1, width=3, bg=black, activebackground=lblack)
-        button_div = tk.Button(root, text="/", command=self.div, height=1, width=3, bg=black, activebackground=lblack)
-        button_eq = tk.Button(root, text="=", command=self.equals, height=1, width=3, bg=black, activebackground=lblack)
-        button_ac = tk.Button(root, text="AC", command=self.clear, height=1, width=3, bg=red, activebackground=lred)
-        button_del = tk.Button(root, text="DEL", command=self.delete, height=1, width=3, bg=red, activebackground=lred)
-        button_op = tk.Button(root, text="(", command=op, height=1, width=3, bg=black, activebackground=lblack)
-        button_cp = tk.Button(root, text=")", command=cp, height=1, width=3, bg=black, activebackground=lblack)
-        button_pow = tk.Button(root, text="aᵇ", command=self._pow, height=1, width=3, bg=black, activebackground=lblack)
-        button_sqrt = tk.Button(root, text="√", command=self.sqrt, height=1, width=3, bg=black, activebackground=lblack)
-        button_ln = tk.Button(root, text="ln", command=self.ln, height=1, width=3, bg=black, activebackground=lblack)
-        button_log = tk.Button(root, text="log", command=self.log, height=1, width=3, bg=black, activebackground=lblack)
-        button_cm = tk.Button(root, text=",", command=cm, height=1, width=3, bg=black, activebackground=lblack)
-        button_plt = tk.Button(root, text="plot", command=self.plot, height=1, width=3, bg=black, activebackground=lblack)
-        button_e = tk.Button(root, text="e", command=_e, heigh=1, width=3, bg=black, activebackground=lblack)
-        button_pi = tk.Button(root, text="π", command=_pi, heigh=1, width=3, bg=black, activebackground=lblack)
-        button_fac = tk.Button(root, text="n!", command=self.fac, height=1, width=3, bg=black, activebackground=lblack)
-        button_x = tk.Button(root, text="x", command=_x, height=1, width=3, bg=black, activebackground=lblack)
+        button_shift = tk.Button(root, text="shift", command=self.shift, height=1, width=3, fg=yellow, bg=black, activeforeground=lyellow, activebackground=lblack)
+        button_hyp = tk.Button(root, text="hyp", command=self.hyperbolic, height=1, width=3, fg=blue, bg=black, activeforeground=lblue, activebackground=lblack)
+        button_sine = tk.Button(root, text="sin", command=self.sin, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_cosine = tk.Button(root, text="cos", command=self.cos, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_tangent = tk.Button(root, text="tan", command=self.tan, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        
+        button_dot = tk.Button(root, text=".", command=self.dot, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_sign = tk.Button(root, text="+/-", command=self.sign, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_plus = tk.Button(root, text="+", command=self.plus, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_minus = tk.Button(root, text="-", command=self.minus, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_mult = tk.Button(root, text="×", command=self.mult, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_div = tk.Button(root, text="/", command=self.div, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_eq = tk.Button(root, text="=", command=self.equals, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_ac = tk.Button(root, text="AC", command=self.clear, height=1, width=3, fg=red, bg=black, activeforeground=lred, activebackground=lblack)
+        button_del = tk.Button(root, text="DEL", command=self.delete, height=1, width=3, fg=red,  bg=black, activeforeground=lred, activebackground=lblack)
+        button_op = tk.Button(root, text="(", command=op, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_cp = tk.Button(root, text=")", command=cp, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_pow = tk.Button(root, text="aᵇ", command=self._pow, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_abs = tk.Button(root, text="abs", command=abs, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_sqrt = tk.Button(root, text="√", command=self.sqrt, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_ln = tk.Button(root, text="ln", command=self.ln, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_log = tk.Button(root, text="log", command=self.log, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_cm = tk.Button(root, text=",", command=cm, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_plt = tk.Button(root, text="plot", command=self.plot, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_e = tk.Button(root, text="e", command=_e, heigh=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_pi = tk.Button(root, text="π", command=_pi, heigh=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_fac = tk.Button(root, text="n!", command=self.fac, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        button_x = tk.Button(root, text="x", command=_x, height=1, width=3, fg=white, bg=black, activeforeground=lwhite, activebackground=lblack)
+        
+        button_shift.place(x=10, y=100)
+        button_hyp.place(x=70, y=100)
+        button_sine.place(x=130, y=100)
+        button_cosine.place(x=190, y=100)
+        button_tangent.place(x=250, y=100)
 
-        button_e.place(x=10, y=260)
-        button_pi.place(x=70, y=260)
-        button_dot.place(x=70, y=220)
-        button_sign.place(x=130, y=220)
-        button_plus.place(x=190, y=100)
-        button_minus.place(x=190, y=140)
-        button_mult.place(x=190, y=180)
-        button_div.place(x=190, y=220)
-        button_eq.place(x=190, y=260)
-        button_plt.place(x=190, y=300)
-        button_ac.place(x=250, y=100)
-        button_op.place(x=310, y=100)
-        button_cp.place(x=370, y=100)
-        button_pow.place(x=250, y=220)
-        button_sqrt.place(x=250, y=180)
-        button_ln.place(x=310, y=140)
-        button_log.place(x=370, y=140)
-        button_cm.place(x=310, y=180)
-        button_del.place(x=250, y=140)
-        button_fac.place(x=310, y=220)
-        button_x.place(x=130, y=260)
+        button_plus.place(x=190, y=140)
+        button_ac.place(x=250, y=140)
+        button_op.place(x=310, y=140)
+        button_cp.place(x=370, y=140)
+
+        button_minus.place(x=190, y=180)
+        button_del.place(x=250, y=180)
+        button_ln.place(x=310, y=180)
+        button_log.place(x=370, y=180)
+
+        button_mult.place(x=190, y=220)
+        button_sqrt.place(x=250, y=220)
+        button_cm.place(x=310, y=220)
+        button_abs.place(x=370, y=220)
+
+        button_dot.place(x=70, y=260)
+        button_sign.place(x=130, y=260)
+        button_div.place(x=190, y=260)
+        button_pow.place(x=250, y=260)
+        button_fac.place(x=310, y=260)
+
+        button_e.place(x=10, y=300)
+        button_pi.place(x=70, y=300)
+        button_x.place(x=130, y=300)
+        button_eq.place(x=190, y=300)
+        
+        button_plt.place(x=190, y=340)
 
 b = Buttons(key_buffer)
 
